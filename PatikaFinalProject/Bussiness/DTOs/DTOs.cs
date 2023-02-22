40fa81d1d9fb9d294237a6a3ba5df36ac0ad760a
Tahir Özdilek <storm_999_@hotmail.com>
2023-02-22 11:46:38 +0300

    update

--- PatikaFinalProject/Bussiness/DTOs/DTOs.cs
+++ PatikaFinalProject/Bussiness/DTOs/DTOs.cs
@@ -3,92 +3,57 @@ using System.Data.SqlTypes;
 
 namespace PatikaFinalProject.DataAccess
 {
-    public class DirectorDTO
+    public class ShoppingListDTO
+    {
+        public int ID { get; set; }
+
+        public int CategoryID { get; set; }
+        [ForeignKey("CategoryID")]
+        public Category Category { get; set; }
+
+        public ICollection<Product> ProductList { get; set; }
+
+        public bool isBought { get; set; }
+        public DateOnly CreationDate { get; set; }
+        public DateOnly CompletedDate { get; set; }
+    }
+    public class ShoppingListCreateDTO
+    {
+        public int CategoryID { get; set; }
+        public bool isBought { get; set; }
+        public DateOnly CreationDate { get; set; }
+        public DateOnly CompletedDate { get; set; }
+    }
+
+    public class CategoryDTO
     {
         public int ID { get; set; }
         public string Name { get; set; }
-        public string Surname { get; set; }
+        public string Description { get; set; }
     }
-    public class DirectorCreateDTO
+    public class CategoryCreateDTO
     {
         public string Name { get; set; }
-        public string Surname { get; set; }
+        public string Description { get; set; }
     }
 
-    public class ActorDTO
+    public class ProductDTO
     {
         public int ID { get; set; }
+
+        public int ShoppingListID { get; set; }
+        [ForeignKey("ShoppingListID")]
+        public ShoppingList ShoppingList { get; set; }
+
         public string Name { get; set; }
-        public string Surname { get; set; }
-    }    
-    public class ActorCreateDTO
+        public string Unit { get; set; }
+        public int Amount { get; set; }
+    }
+    public class ProductCreateDTO
     {
+        public int ShoppingListID { get; set; }
         public string Name { get; set; }
-        public string Surname { get; set; }
-    }
-
-    public class MovieDTO
-    {
-        public int ID { get; set; }
-        public string MovieName { get; set; }
-        public int MovieTypeID { get; set; }
-        [ForeignKey("MovieTypeID")]
-        public MovieType? MovieType { get; set; }
-        public DateTime MovieYear { get; set; }
-        public int DirectorID { get; set; }
-        [ForeignKey("DirectorID")]
-        public Director? Director { get; set; }
-        public int Price { get; set; }
-        public bool IsSold { get; set; } = false;
-    }
-    public class MovieCreateDTO
-    {
-        public string MovieName { get; set; }
-        public int MovieTypeID { get; set; }
-        public DateTime MovieYear { get; set; }
-        public int DirectorID { get; set; }
-        public int Price { get; set; }
-    }
-
-    public class MovieTypeDTO
-    {
-        //enum MovieTypes { Comedy = 1, Horror, Action};
-        public int ID { get; set; }
-        public string Type { get; set; }
-    }
-
-
-    public class CustomerCreateDTO
-    {
-        public string Name { get; set; }
-        public string Surname { get; set; }
-    }
-
-    public class OrderCreateDTO
-    {
-        public DateTime OrderDate { get; set; }
-        public int MovieID { get; set; }
-        public int CustomerID { get; set; }
-        public int Price { get; set; }
-    }
-
-    public class ActorMovieDTO
-    {
-        public int ActorID { get; set; }
-        [ForeignKey("ActorID")]
-        public Actor? Actor { get; set; }
-        public int MovieID { get; set; }
-        [ForeignKey("MovieID")]
-        public Movie? Movie { get; set; }
-    }
-
-    public class CustomerFavTypeDTO
-    {
-        public int CustomerID { get; set; }
-        [ForeignKey("CustomerID")]
-        public Customer? Customer { get; set; }
-        public int FavMovieTypeID { get; set; }
-        [ForeignKey("FavMovieTypeID")]
-        public MovieType? MovieType { get; set; }
+        public string Unit { get; set; }
+        public int Amount { get; set; }
     }
 }
