55cf627e513d7230e182a1e830681fe3f83e4936
Tahir Özdilek <storm_999_@hotmail.com>
2023-02-22 11:06:30 +0300

    update

--- PatikaFinalProject/DataAccess/Models.cs
+++ PatikaFinalProject/DataAccess/Models.cs
@@ -1,21 +1,44 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using Azure;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.SqlTypes;
 
 namespace PatikaFinalProject.DataAccess
 {
-    public class Director
+    public class ShoppingList
     {
         public int ID { get; set; }
-        public string Name { get; set; }
-        public string Surname { get; set; }
+        
+        public int CategoryID { get; set; }
+        [ForeignKey("CategoryID")]
+        public Category Category { get; set; }
+
+        public ICollection<Product> ProductList { get; set; }
+        
+        public bool isBought { get; set; }
+        public DateOnly CreationDate { get; set; }
+        public DateOnly CompletedDate { get; set; }
     }
 
-    public class Actor
+    public class Category
     {
         public int ID { get; set; }
         public string Name { get; set; }
-        public string Surname { get; set; }
+        public string Description { get; set; }
     }
+
+    public class Product
+    {
+        public int ID { get; set; }
+
+        public int ShoppingListID { get; set; }
+        [ForeignKey("ShoppingListID")]
+        public ShoppingList ShoppingList { get; set; }
+
+        public string Name { get; set; }
+        public string Unit { get; set; }
+        public int Amount { get; set; }
+    }
+
 
     public class Movie
     {
@@ -32,12 +55,7 @@ namespace PatikaFinalProject.DataAccess
         public bool IsSold { get; set; } = false;
     }
 
-    public class MovieType
-    {
-        //enum ID { Comedy = 1, Horror, Action};
-        public int ID { get; set; }
-        public string Type { get; set; }
-    }
+
 
 
     public class Customer
