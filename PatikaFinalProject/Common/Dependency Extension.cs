ec3d1ba71165c30a1777fa1a8d8a17798e7d300f
storm999 <storm_999_@hotmail.com>
2023-03-02 11:36:01 +0300

    dependency extension updated

--- PatikaFinalProject/Common/Dependency Extension.cs
+++ PatikaFinalProject/Common/Dependency Extension.cs
@@ -1,15 +1,19 @@
 ﻿using AutoMapper;
 using FluentValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Cors.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using PatikaFinalCase.Bussiness.Services;
 using PatikaFinalProject.Bussiness.Services;
 using PatikaFinalProject.Controllers;
 using PatikaFinalProject.DataAccess;
 using PatikaFinalProject.Services.Mapper;
 using PatikaFinalProject.Services.Validators;
-
+using System.Text;
+using System.Text.Json.Serialization;
 
 namespace PatikaFinalProject.Common
 {
@@ -17,6 +21,40 @@ namespace PatikaFinalProject.Common
     {
         public static void AddDependencies(this IServiceCollection services)
         {
+            services.AddControllers()
+            .AddJsonOptions(o => o.JsonSerializerOptions
+                .ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+            services.AddSwaggerGen(c => {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                                    {
+                                        Title = "JWTToken_Auth_API",
+                                        Version = "v1"
+                                    });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                                        {
+                                            Name = "Authorization",
+                                            Type = SecuritySchemeType.ApiKey,
+                                            Scheme = "Bearer",
+                                            BearerFormat = "JWT",
+                                            In = ParameterLocation.Header,
+                                            Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer exzcxv...\"",
+                                        });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement 
+                                        {
+                                            {
+                                                new OpenApiSecurityScheme {
+                                                    Reference = new OpenApiReference {
+                                                        Type = ReferenceType.SecurityScheme,
+                                                            Id = "Bearer"
+                                                    }
+                                                },
+                                                new string[] {}
+                                            }
+                                        });
+            });
+            services.AddOutputCache();
+
             MapperConfiguration configuration = new MapperConfiguration(opt =>
             {
                 opt.AddProfile(new ShoppingListProfile());
@@ -47,6 +85,33 @@ namespace PatikaFinalProject.Common
             services.AddScoped<RegisterLoginService, RegisterLoginService>();
             services.AddScoped<ShoppingListService, ShoppingListService>();
             services.AddScoped<ShoppingListSearchService, ShoppingListSearchService>();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy(name: "_myAllowSpecificOrigins",
+                   builder =>
+                   {
+                       builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost");
+                   });
+            });
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt => {
+                opt.RequireHttpsMetadata = false;
+                opt.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidIssuer = "http://localhost",
+                    ValidAudience = "http://localhost",
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("How much is this static key secure ?")),
+                    ValidateIssuerSigningKey = true,
+                    ValidateLifetime = true,
+                    ClockSkew = TimeSpan.Zero,
+                };
+            });
+
+            services.AddOutputCache(options =>
+            {
+                options.DefaultExpirationTimeSpan = TimeSpan.FromMinutes(10);
+            });
         }
     }
 }
--- PatikaFinalProject/Program.cs
+++ PatikaFinalProject/Program.cs
@@ -9,66 +9,13 @@ using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddControllers()
-            .AddJsonOptions(o => o.JsonSerializerOptions
-                .ReferenceHandler = ReferenceHandler.IgnoreCycles);
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(c => {
-    c.SwaggerDoc("v1", new OpenApiInfo
-    {
-        Title = "JWTToken_Auth_API",
-        Version = "v1"
-    });
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.ApiKey,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1safsfsdfdfd\"",
-    });
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement {
-        {
-            new OpenApiSecurityScheme {
-                Reference = new OpenApiReference {
-                    Type = ReferenceType.SecurityScheme,
-                        Id = "Bearer"
-                }
-            },
-            new string[] {}
-        }
-    });
-});
-builder.Services.AddOutputCache();
 
 builder.Services.AddDependencies();
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy(name: "_myAllowSpecificOrigins",
-       builder =>
-       {
-           builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost");
-       });
-});
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(opt => {
-    opt.RequireHttpsMetadata = false;
-    opt.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
-    {
-        ValidIssuer = "http://localhost",
-        ValidAudience = "http://localhost",
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("How much is this static key secure ?")),
-        ValidateIssuerSigningKey = true,
-        ValidateLifetime = true,
-        ClockSkew = TimeSpan.Zero,
-    };
-});
-builder.Services.AddOutputCache(options =>
-{
-    options.DefaultExpirationTimeSpan = TimeSpan.FromMinutes(10);
-});
+
 var app = builder.Build();
 
 app.UseOutputCache();
